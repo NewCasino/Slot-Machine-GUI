738eeb79c0b0362611aa2ad912a821edd7f6adc9
kalsara <kalsaramagamage@gmail.com>
2017-02-24 08:31:57 +0530

    Isymbol interface

---
+++ src/GUI/Isymbol.java
@@ -0,0 +1,14 @@
+package src.GUI;
+import java.awt.*;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+
+ interface Isymbol {
+	public void setImage(String location,String name);
+	public ImageIcon getImage();
+	public void setValue(int value);
+	public int getValue();
+	
+
+}

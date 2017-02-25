b4dc4f876ff7e8ea4e4939a5d4fd43ce06ac15df
kalsara <kalsaramagamage@gmail.com>
2017-02-25 22:34:08 +0530

    symbol class

---
+++ src/GUI/symbol.java
@@ -0,0 +1,31 @@
+package src.GUI;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+
+public class symbol implements Isymbol {
+
+	ImageIcon img = new ImageIcon();
+	int value = 0;
+
+	@Override
+	public void setImage(String location, String name) {
+		img = createImageIcon(location, name);
+
+	}
+
+	public ImageIcon createImageIcon(String location, String name) {
+		java.net.URL imgURL = getClass().getResource(location);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL, name);
+		} else {
+			System.err.println("Couldn't find file: " + location);
+			return null;
+		}
+
+	}
+
+
+}
